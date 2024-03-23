a3182f7e0d697b23722f88f5a20bd92278477722
Kaleb Schwart <kaleb.schwart@gmail.com>
2024-03-23 17:17:14 -0500

    adding skeleton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from skills import Skills
 
-spore_bomb = Skills(DMG=229)
-
+spore_bomb = Skills(DMG=68)
 print(f"trigger an explosion on the target 2 time(s), each time dealing {spore_bomb.DMG}% DMG")
+
