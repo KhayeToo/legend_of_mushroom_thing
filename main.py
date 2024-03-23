f7f9bcffad5ab1a93c6f635025adaa69eabfc16f
Kaleb Schwart <kaleb.schwart@gmail.com>
2024-03-23 17:20:21 -0500

    cpital T

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from skills import Skills
 
 spore_bomb = Skills(DMG=68)
-print(f"trigger an explosion on the target 2 time(s), each time dealing {spore_bomb.DMG}% DMG")
+print(f"Trigger an explosion on the target 2 time(s), each time dealing {spore_bomb.DMG}% DMG")
 
