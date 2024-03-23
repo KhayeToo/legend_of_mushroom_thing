9a2483a09eb4a78422ea06b83d2bcdad61e95bbc
Connor Wehrum <connor12568@gmail.com>
2024-03-23 18:11:53 -0400

    adding req.txt and fixing skill bug

--- skills.py
+++ skills.py
@@ -3,27 +3,27 @@ from typing import Optional
 
 class Skills(BaseModel):
     DMG: int
-    Slow: Optional[int]
-    Increase_Basic_Attack: Optional[int]
-    Imprison: Optional[int]
-    Increase_Attack_Speed: Optional[int]
-    Heal: Optional[int]
-    Shield: Optional[int]
-    Reduce_Attack: Optional[int]
-    Reduce_Attack_Speed: Optional[int]
-    Increase_Pal_DMG: Optional[int]
-    Increase_Skill_DMG: Optional[int]
-    Disarm: Optional[int]
-    Stun: Optional[int]
-    Increase_DMG_Recieved: Optional[int]
-    Instant_Defeat: Optional[int]
-    DMG_Immunity: Optional[int]
-    Lose_Max_Health_Per_Second: Optional[int]
-    Clone: Optional[int]
-    Basic_attack_DMG_RES: Optional[int]
-    ATK_Bonus: Optional[int]
-    Vulnerability: Optional[int]
-    Inncrease_Crit_Rate: Optional[int]
-    Increase_Crit_DMG: Optional[int]
-    Increase_Skill_Crit_Rate: Optional[int]
-    Increase_Skill_Crit_DMG: Optional[int]    
+    Slow: Optional[int] = None
+    Increase_Basic_Attack: Optional[int] = None
+    Imprison: Optional[int] = None
+    Increase_Attack_Speed: Optional[int] = None
+    Heal: Optional[int] = None
+    Shield: Optional[int] = None
+    Reduce_Attack: Optional[int] = None
+    Reduce_Attack_Speed: Optional[int] = None
+    Increase_Pal_DMG: Optional[int] = None
+    Increase_Skill_DMG: Optional[int] = None
+    Disarm: Optional[int] = None
+    Stun: Optional[int] = None
+    Increase_DMG_Recieved: Optional[int] = None
+    Instant_Defeat: Optional[int] = None
+    DMG_Immunity: Optional[int] = None
+    Lose_Max_Health_Per_Second: Optional[int] = None
+    Clone: Optional[int] = None
+    Basic_attack_DMG_RES: Optional[int] = None
+    ATK_Bonus: Optional[int] = None
+    Vulnerability: Optional[int] = None
+    Inncrease_Crit_Rate: Optional[int] = None
+    Increase_Crit_DMG: Optional[int] = None
+    Increase_Skill_Crit_Rate: Optional[int] = None
+    Increase_Skill_Crit_DMG: Optional[int] = None 
