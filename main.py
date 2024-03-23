b49edde351a3574617136c56629fc9e4ef675579
Kaleb Schwart <kaleb.schwart@gmail.com>
2024-03-23 17:30:13 -0500

    src folder works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from skills import Skills
+from src.equipment.skills import Skills
 
 spore_bomb = Skills(DMG=68)
 print(f"Trigger an explosion on the target 2 time(s), each time dealing {spore_bomb.DMG}% DMG")
